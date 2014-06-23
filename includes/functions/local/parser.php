296b29f4c47ceaa44707735a743c38e7fda64eea
Boris Penev <bob53181@yahoo.com>
2014-06-23 03:54:01 +0100

    Refactoring the parser All the code is put in functions It looks a lot better, and it easier to change and maintain

--- includes/functions/local/parser.php
+++ includes/functions/local/parser.php
@@ -1,5 +1,21 @@
 <?php
 
+  $club_init = [
+    'name' => '',
+    'address' => '',
+    'postcode' => '',
+    'latitude' => '',
+    'longtitude' => '',
+    'website' => '',
+    'email' => '',
+    'phone' => '',
+    'comment' => '',
+    'opening_time' => '',
+    'closing_time' => '',
+    'price_member' => '',
+    'price_nonmember' => ''
+  ];
+
   function curl_get_file_contents_custom($URL)
   {
     $c = curl_init();
@@ -45,6 +61,140 @@
   {
     return count ( $entity ) > 0 ?
               (string) $entity[0] : '';
+  }
+  
+  function buildQuery ( $entity )
+  {
+    if ( $entity == 'club' )
+    {
+      $club = wh_db_get_input_string ( 'club' );
+      if ( ! isset ( $club ) ) {
+        return '';
+      }
+      return "[title[text()='$club']]";
+    }
+    else if ( $entity == 'sport' )
+    {
+      $sports = wh_db_get_input_string ( 'sport' );
+      if ( isset ( $sports ) ) {
+        return '';
+      }
+      $sports = explode ( ' ', $sports );
+      $query = "[fields/field[@name='Activities']";
+      foreach ( $sports as $sport ) {
+        $query .= "[contains(text(),'$sport')]";
+      }
+      $query .= ']';
+      return $query;
+    }
+    else if ( $entity == 'time' )
+    {
+      $times = wh_db_get_input_string ( 'time' );
+      if ( isset ( $times ) ) {
+        return '';
+      }
+      $times = explode ( ' ', $times );
+      $query = "[fields/field[@name='Opening hours']";
+      foreach ( $times as $time ) {
+        $query .= "[contains(text(),'$time')]";
+      }
+      $query .= ']';
+      return $query;
+    }
+    return '';
+  }
+  
+  function loadXML ( )
+  {
+    $xml = curl_get_html_file_contents_custom (
+      'http://www.edinburgh.gov.uk/api/directories/25/entries.xml?api_key=' .
+      COUNCIL_API_KEY . '&per_page=100&page=1' );
+
+    return new SimpleXMLElement($xml);
+  }
+  
+  function process_current_club ( $club )
+  {
+    $current = $club_init;
+    $address = $club->xpath('fields/field[@name=\'Address\']/text()');
+    $postcode = $club->xpath('fields/field[@name=\'Postcode\']/text()');
+    $location = $club->xpath('fields/field[@name=\'Location\']/text()');
+    $email = $club->xpath('fields/field[@name=\'Email\']/text()');
+    $phone = $club->xpath('fields/field[@name=\'Telephone\']/text()');
+    $sports = $club->xpath('fields/field[@name=\'Activities\']/text()');
+    $facilities = $club->xpath('fields/field[@name=\'Facilities\']/text()');
+    $times = $club->xpath('fields/field[@name=\'Opening hours\']/text()');
+    $prices = $club->xpath('fields/field[@name=\'Prices\']/text()');
+    $website = $club->xpath('fields/field[@name=\'Timetables\']/text()');
+    $comment = $club->xpath('fields/field[@name=\'More information\']/text()');
+    $current['name'] = (string) $club->title;
+
+    if ( count ( $location ) > 0 && strlen ( $location[0] ) > 1 )
+    {
+      $location = explode ( ',', $location[0] );
+      if ( count ( $location ) == 2 )
+      {
+        $latitude = $location [ 0 ];
+        $longtitude = $location [ 1 ];
+        $current['latitude'] = (double) $latitude;
+        $current['longtitude'] = (double) $longtitude;
+      }
+    }
+#   echo $club->title, '  at  address ', $address [0];
+    $current['address'] = get_first_element ($address);
+    $current['postcode'] = get_first_element ($postcode);
+    $current['email'] = get_first_element ($email);
+    $current['phone'] = get_first_element ($phone);
+    $current['website'] = get_first_element ($website);
+    $sports = get_first_element ($sports);
+    $facilities = get_first_element ($facilities);
+    $times = get_first_element ($times);
+    $prices = get_first_element ($prices);
+    $comment = get_first_element ($comment);
+    $sports = str_replace ( "\r", '', $sports );
+    $sports = str_replace ( "\n", ', ', $sports );
+    $facilities = str_replace ( "\r", '', $facilities );
+    $facilities = str_replace ( "\n", ', ', $facilities );
+    $times = str_replace ( "\r", '', $times );
+#   $times = str_replace ( "\n", ', ', $times );
+    $prices = str_replace ( "\r", '', $prices );
+    $prices = str_replace ( "\n", ', ', $prices );
+#   $comment = str_replace ( "\r", '', $comment );
+#   $comment = str_replace ( "\n", ', ', $comment );
+    $current['sports'] = $sports;
+    $current['facilities'] = $facilities;
+    $current['time'] = $times;
+    $current['price'] = $prices;
+    $current['comment'] = $comment;
+    /*if ( isset ( $time_var ) )
+      {
+      //echo 'at times ',  implode ( ' ', $times ),
+      //'<br />', PHP_EOL;
+      $current['time'] = implode ( ' ', $times );
+    }
+    else
+    {
+      $current['time'] = '';
+    }*/
+    //$arr[$count++] = $current;
+    return $current;
+  }
+
+  function process_clubs ( $xml, $query )
+  {
+    foreach ( $xml->xpath('/entries/entry' . $query) as $club )
+    {
+      $current_club = process_current_club ($club);
+      $arr[] = $current_club;
+      var_dump ($current_club);
+    }
+  }
+
+  function output_array ( $arr )
+  {
+#   echo json_encode ( $arr );
+    var_dump ($arr);
+#   var_export($arr);
   }
 
 ?>
