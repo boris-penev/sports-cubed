dbce1cafe56dab3c6b2acd3cd46810e8fd0a31f3
Boris Penev <bob53181@yahoo.com>
2014-07-27 18:46:33 +0100

    Whitespace changes
    Replace tabs with 2 spaces in all source files.
    .html, .js, .css files affected by the change

--- includes/general-local.js
+++ includes/general-local.js
@@ -3,38 +3,38 @@
 */
 function hideNotification ( )
 {
-	setTimeout ( function() {
-			var array = document.getElementsByClassName("notification");
-			for (i=0;i<array.length;i++) {
-				array[i].style.visibility = 'hidden';
-			}
-		}, 2000 );
+  setTimeout ( function() {
+      var array = document.getElementsByClassName("notification");
+      for (i=0;i<array.length;i++) {
+        array[i].style.visibility = 'hidden';
+      }
+    }, 2000 );
 }
 
 function setGlobalOpen(){
-	var val = document.getElementById("time_open_global").value;
-	var elements = document.getElementsByClassName('selectSport');
-	for(var x in elements){
-		if(elements[x].checked === true){
-			for(i=1; i<=7; i++){
-			id=elements[x].getAttribute('id').substring(12,elements[x].getAttribute('id').length);
-			document.getElementById("timeOpenDay"+i+"_"+id).value = val;
-			}
-		}
-	}
+  var val = document.getElementById("time_open_global").value;
+  var elements = document.getElementsByClassName('selectSport');
+  for(var x in elements){
+    if(elements[x].checked === true){
+      for(i=1; i<=7; i++){
+      id=elements[x].getAttribute('id').substring(12,elements[x].getAttribute('id').length);
+      document.getElementById("timeOpenDay"+i+"_"+id).value = val;
+      }
+    }
+  }
 }
 
 function setGlobalClose(){
-	var val = document.getElementById("time_close_global").value;
-	var elements = document.getElementsByClassName('selectSport');
-	for(var x in elements){
-		if(elements[x].checked === true){
-			for(i=1; i<=7; i++){
-			id=elements[x].getAttribute('id').substring(12,elements[x].getAttribute('id').length);
-			document.getElementById("timeCloseDay"+i+"_"+id).value = val;
-			}
-		}
-	}
+  var val = document.getElementById("time_close_global").value;
+  var elements = document.getElementsByClassName('selectSport');
+  for(var x in elements){
+    if(elements[x].checked === true){
+      for(i=1; i<=7; i++){
+      id=elements[x].getAttribute('id').substring(12,elements[x].getAttribute('id').length);
+      document.getElementById("timeCloseDay"+i+"_"+id).value = val;
+      }
+    }
+  }
 }
 
 document.getElementById("time_open_global_submit").onclick=function(){setGlobalOpen()};
