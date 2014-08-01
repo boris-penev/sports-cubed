b4a9fa5a0e0912cb5bd89b95efc6bda922336a24
Boris Penev <bob53181@yahoo.com>
2014-08-01 02:41:46 +0100

    Rename URL_XML_COUNCIL to URL_XML_COUNCIL_EDINBURGH

--- parser.php
+++ parser.php
@@ -3,7 +3,7 @@
   require('includes/application_top.php');
   require(DIR_WS_FUNCTIONS . 'local/parser.php');
 
-  $xml = loadXML ( URL_XML_COUNCIL );
+  $xml = loadXML ( URL_XML_COUNCIL_EDINBURGH );
   $query = build_query_council_edinburgh ( 'club' );
 //  $query = buildQuery ( 'sport' ) . buildQuery ( 'time' );
   delete_clubs ();
