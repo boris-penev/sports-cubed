6421f2702cc6d7204ff6d0404efd5ae078e24f3d
Boris Penev <bob53181@yahoo.com>
2014-08-14 23:39:02 +0300

    Table prefix support
    Add table prefix so the site can be hosted on a shared database.
    
    Example prefix configuration:
    define('DB_TABLE_PREFIX', 'sc_');
    
    Underscore is not obligatory.

--- includes/database_tables.php
+++ includes/database_tables.php
@@ -11,19 +11,19 @@
 */
 
 // define the database table names used in the project
-  define('TABLE_SPORTS', 'sports');
-  define('TABLE_FACILITIES', 'facilities');
-  define('TABLE_DAYS', 'days');
-  define('TABLE_CLUBS_PRODUCTION', 'clubs');
+  define('TABLE_SPORTS', DB_TABLE_PREFIX . 'sports');
+  define('TABLE_FACILITIES', DB_TABLE_PREFIX . 'facilities');
+  define('TABLE_DAYS', DB_TABLE_PREFIX . 'days');
+  define('TABLE_CLUBS_PRODUCTION', DB_TABLE_PREFIX . 'clubs');
   define('TABLE_CLUBS', TABLE_CLUBS_PRODUCTION . '_tmp');
   define('TABLE_CLUBS_OLD', TABLE_CLUBS_PRODUCTION . '_old');
-  define('TABLE_CLUB_SCHEDULE_PRODUCTION', 'club_schedule');
+  define('TABLE_CLUB_SCHEDULE_PRODUCTION', DB_TABLE_PREFIX . 'club_schedule');
   define('TABLE_CLUB_SCHEDULE', TABLE_CLUB_SCHEDULE_PRODUCTION . '_tmp');
   define('TABLE_CLUB_SCHEDULE_OLD', TABLE_CLUB_SCHEDULE_PRODUCTION . '_old');
-  define('TABLE_CLUBOSPORT_PRODUCTION', 'clubosport');
+  define('TABLE_CLUBOSPORT_PRODUCTION', DB_TABLE_PREFIX . 'clubosport');
   define('TABLE_CLUBOSPORT', TABLE_CLUBOSPORT_PRODUCTION . '_tmp');
   define('TABLE_CLUBOSPORT_OLD', TABLE_CLUBOSPORT_PRODUCTION . 'old');
-  define('TABLE_CLUB_FACILITIES_PRODUCTION', 'club_facilities');
+  define('TABLE_CLUB_FACILITIES_PRODUCTION', DB_TABLE_PREFIX.'club_facilities');
   define('TABLE_CLUB_FACILITIES', TABLE_CLUB_FACILITIES_PRODUCTION . '_tmp');
   define('TABLE_CLUB_FACILITIES_OLD', TABLE_CLUB_FACILITIES_PRODUCTION .'_old');
 ?>
